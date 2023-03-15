9845874949ca4f42b5e5f60e56128eefb47ca31a
Incinerator67 <edemosmanov2699@gmail.com>
2023-03-15 14:21:05 +0300

    четырнадцатые десять заданий

--- 315/Users.js
+++
@@ -1,7 +0,0 @@
-let elem = document.querySelector('#elem');
-
-elem.addEventListener('click', function() {
-	setInterval(function(self) {
-		console.log(self.value);
-	}, 1000, this);
-});
--- 318/Users.js
+++
@@ -1,6 +0,0 @@
-let parent = document.querySelector('#parent');
-
-let p = document.createElement('p');
-p.textContent = '!';
-
-parent.appendChild(p);
